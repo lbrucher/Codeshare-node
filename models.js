ced52d629e5b5671ae2d9cf04adc1d2c6c1a32ca
Laurent Brucher <laurent.brucher@idelis.be>
2011-10-03 20:40:43 -0400

    made new model work...

--- models.js
+++ models.js
@@ -2,18 +2,19 @@ var crypto = require('crypto');
 
 
 function defineModels(mongoose, callback) {
-  var Schema = mongoose.Schema,
-      ObjectId = Schema.ObjectId;
+	var   Schema = mongoose.Schema
+		, ObjectId = Schema.ObjectId
+		;
 
 
 	// ----------------------------------------
 	// SAVED TEXT
 	// ----------------------------------------
-  SavedTextSchema = new Schema({
-      'name': { type: String, required: true, index: { unique: true } }
-    , 'description': String
-    , 'content': String
-  });
+	SavedTextSchema = new Schema({
+		  'name': { type: String, required: true, index: { unique: true } }
+		, 'description': String
+		, 'content': String
+	});
 
 	mongoose.model('SavedTextSchema', SavedTextSchema);
 
@@ -21,10 +22,10 @@ function defineModels(mongoose, callback) {
 	// ----------------------------------------
 	// USER GROUP
 	// ----------------------------------------
-  UserGroupSchema = new Schema({
-      'name': { type: String, required: true, index: { unique: true } }
-    , 'saved_texts': [SavedTextSchema]		// TODO: replace with DBRefs
-  });
+	UserGroupSchema = new Schema({
+		  'name': { type: String, required: true, index: { unique: true } }
+		, 'saved_texts': [SavedTextSchema]		// TODO: replace with DBRefs
+	});
 
 	mongoose.model('UserGroupSchema', UserGroupSchema);
 
@@ -32,34 +33,34 @@ function defineModels(mongoose, callback) {
 	// ----------------------------------------
 	// USER
 	// ----------------------------------------
-  UserSchema = new Schema({
-    'username': { type: String, required: true, index: { unique: true } },
-    'password_hashed': String,
-    'first_name': String,
-    'last_name': String,
-    'salt': String,
-    'group': UserGroupSchema
-  });
+	UserSchema = new Schema({
+		  'username': { type: String, required: true, index: { unique: true } }
+		, 'password_hashed': String
+		, 'first_name': String
+		, 'last_name': String
+		, 'salt': String
+		, 'group': [UserGroupSchema]
+	});
 
-  UserSchema.virtual('password')
+	UserSchema.virtual('password')
 		.set(function(password) {
 			this._password = password;
-      this.salt = this.makeSalt();
+			this.salt = this.makeSalt();
 			this.password_hashed = this.encryptPassword(password);
-    })
-    .get(function() { return this._password; });
+		})
+		.get(function() { return this._password; });
 
-  UserSchema.method('makeSalt', function() {
-    return Math.round((new Date().valueOf() * Math.random())) + '';
-  });
+	UserSchema.method('makeSalt', function() {
+		return Math.round((new Date().valueOf() * Math.random())) + '';
+	});
 
 	UserSchema.method('authenticate', function(pwdClear) {
-	    return this.encryptPassword(pwdClear) === this.password_hashed;
+		return this.encryptPassword(pwdClear) === this.password_hashed;
 	});
 	
-  UserSchema.method('encryptPassword', function(pwdClear) {
-    return crypto.createHmac('sha1', this.salt).update(pwdClear).digest('hex');
-  });
+	UserSchema.method('encryptPassword', function(pwdClear) {
+		return crypto.createHmac('sha1', this.salt).update(pwdClear).digest('hex');
+	});
 
 
 	mongoose.model('UserSchema', UserSchema);
