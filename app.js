45bd9bea2e9fdd8f2f03368590ba49b93e4b9288
Laurent Brucher <laurent.brucher@idelis.be>
2011-10-17 19:20:08 -0400

    Merge from Savedtext branch (part2)

--- app.js
+++ app.js
@@ -13,110 +13,20 @@ var User, Session, UserGroup, SavedText;
 var isDebug = false;
 var isHttps = false;
 var autologin = true;
-
-// Application/Server    
 var app;
-if (isHttps) {
-	var serverOptions = {
-		key: fs.readFileSync(__dirname+'/privatekey.pem'),
-		cert: fs.readFileSync(__dirname+'/certificate.pem')
-	};
-	app = express.createServer(serverOptions);
-} else {
-	app = express.createServer();
-}
-module.exports = app;
 
-//var host = process.env.VCAP_APP_HOST || 'localhost';
-var port = Number(process.env.PORT || process.env.VCAP_APP_PORT || 8000);
-
-
-// Configuration
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-	//app.use(express.logger());
-	app.use(stylus.middleware({ src: __dirname + '/public' }));
-  app.use(express.bodyParser());
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: "4roo0cff 3elk" }));
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-
-	sessions.init(app);
-});
-
-app.configure('test', function(){
-  app.set('db-uri', 'mongodb://localhost/codeshare-test');
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-	isDebug = true;
-});
-
-app.configure('development', function(){
-  app.set('db-uri', 'mongodb://localhost/codeshare');
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-//	isDebug = true;
-});
-
-app.configure('production', function(){
-  app.set('db-uri', process.env.MONGOHQ_URL);
-  app.use(express.errorHandler()); 
-	isDebug = false;
-});
-
-
-// This is especially needed for unit tests, or the process will never exit.
-app.on('close', function() {
-	mongoose.disconnect();
-});
-
-
-// MONGOOSE
-models.defineModels(mongoose, function() {
-  app.User = User = mongoose.model('UserSchema');
-//	app.UserGroup = UserGroup = mongoose.model('UserGroupSchema');
-	app.SavedText = SavedText = mongoose.model('SavedTextSchema');
-
-  db = mongoose.connect(app.set('db-uri'));
-
-/*
-	// See if there is an admin group. If not, create a standard one.
-	var adminGroup = null;
-	UserGroup.findOne( {name:'admin'}, function(err,group) {
-		adminGroup = group;
-		if (!group) {
-			// Create the admin group
-			adminGroup = new UserGroup({name:'admin');
-			adminGroup.save(function(err) {
-				if (err)
-					console.log('FAILED creating the admin group: '+err);
-				else
-					console.log('Created the admin group');
-			});
-		}
-	});
-*/
-
-	// See if there is an admin account. If not, create a standard one.
-	User.findOne( {username:'admin'}, function(err,user) {
-		if (!user) {
-			// Create the admin account
-			user = new User({username:'admin', password:'admin', first_name:'super', last_name:'user'});
-			user.save(function(err) {
-				if (err)
-					console.log('FAILED creating the admin account: '+err);
-				else
-					console.log('Created the admin account');
-			});
-		}
-	});
-
-})
+var LOG_ERROR=1, LOG_WARNING=2, LOG_DEBUG=3, LOG_TRACE=4;
 
 
 
+// //////////////////////////////////////////////////////////////////////////
 // HELPERS
+// //////////////////////////////////////////////////////////////////////////
+
+function log(level, message) {
+	if (level <= app.set('log-level'))
+		console.log(message);
+}
 
 function getRefreshedText(aSession, lastUpdateTime, who)
 {
@@ -143,7 +53,7 @@ function isAdmin(req) {
 
 
 function login(req,res,username, password) {
-	console.log('Logging in ['+username+']...');
+	log(LOG_TRACE,'Logging in <'+username+'>...');
 	User.findOne({username:username}, function(err,user) {
 		if (user && user.authenticate(password)) {
 			console.log("login OK for ["+user.username+"]");
@@ -152,7 +62,7 @@ function login(req,res,username, password) {
 			res.redirect('/interviewer');
 		}
 		else {
-			console.log("login FAILED for ["+username+"]");
+			log(LOG_TRACE, 'login FAILED for <'+username+'>');
 			res.render('interviewer/login.jade', {interviewer:true});
 		}
 	});
@@ -214,6 +124,119 @@ function findUserText(user, textId) {
 function isJson(req) {
 	return req.header('Accept', 'text/html').indexOf('application/json') != -1;
 }
+
+
+// //////////////////////////////////////////////////////////////////////////
+// SERVER CREATION
+// //////////////////////////////////////////////////////////////////////////
+
+module.exports.createServer = function() {
+
+	// Application/Server    
+	if (isHttps) {
+		var serverOptions = {
+			key: fs.readFileSync(__dirname+'/privatekey.pem'),
+			cert: fs.readFileSync(__dirname+'/certificate.pem')
+		};
+		console.log("Creating new HTTPS server...");
+		app = express.createServer(serverOptions);
+	} else {
+		console.log("Creating new HTTP server...");
+		app = express.createServer();
+	}
+
+
+	app.shutdown = function(callback) {
+		this.close();
+		callback();
+	}
+
+
+// Configuration
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	//app.use(express.logger());
+	app.use(stylus.middleware({ src: __dirname + '/public' }));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: "4roo0cff 3elk" }));
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+
+	sessions.init(app);
+});
+
+app.configure('test', function(){
+	app.set('log-level', LOG_DEBUG);
+	app.set('db-uri', 'mongodb://localhost/codeshare-test');
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	isDebug = true;
+});
+
+app.configure('development', function(){
+	app.set('log-level', LOG_DEBUG);
+	app.set('db-uri', 'mongodb://localhost/codeshare');
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	isDebug = false;
+});
+
+app.configure('production', function(){
+  app.set('log-level', LOG_WARNING);
+  app.set('db-uri', process.env.MONGOHQ_URL);
+  app.use(express.errorHandler()); 
+	isDebug = false;
+});
+
+
+// This is especially needed for unit tests, or the process will never exit.
+app.on('close', function() {
+	mongoose.disconnect();
+});
+
+
+// MONGOOSE
+models.defineModels(mongoose, function() {
+  app.User = User = mongoose.model('UserSchema');
+//	app.UserGroup = UserGroup = mongoose.model('UserGroupSchema');
+	app.SavedText = SavedText = mongoose.model('SavedTextSchema');
+
+  db = mongoose.connect(app.set('db-uri'));
+
+/*
+	// See if there is an admin group. If not, create a standard one.
+	var adminGroup = null;
+	UserGroup.findOne( {name:'admin'}, function(err,group) {
+		adminGroup = group;
+		if (!group) {
+			// Create the admin group
+			adminGroup = new UserGroup({name:'admin');
+			adminGroup.save(function(err) {
+				if (err)
+					console.log('FAILED creating the admin group: '+err);
+				else
+					console.log('Created the admin group');
+			});
+		}
+	});
+*/
+
+	// See if there is an admin account. If not, create a standard one.
+	User.findOne( {username:'admin'}, function(err,user) {
+		if (!user) {
+			// Create the admin account
+			user = new User({username:'admin', password:'admin', first_name:'super', last_name:'user'});
+			user.save(function(err) {
+				if (err)
+					console.log('FAILED creating the admin account: '+err);
+				else
+					console.log('Created the admin account');
+			});
+		}
+	});
+
+})
 
 
 // ===============================================
@@ -618,6 +641,14 @@ app.get('/interviewer/createNew', secured, function(req,res){
   res.redirect('/interviewer');
 });
 
+app.get('/interviewer/session/:id/details', secured, function(req,res){
+	var s = sessions.get(req.params.id);
+	if (s != null)
+		res.render('interviewer/details.jade', {interviewer:true, currentUser:req.user, session:s});
+	else
+		res.redirect('/interviewer');
+});
+
 app.get('/interviewer/session/:id/close', secured, function(req,res){
 	var s = sessions.get(req.params.id);
 	if (s != null && s.open)
@@ -657,19 +688,35 @@ app.get('/interviewer/session/:id', secured, function(req,res){
 
 
 app.get('/interviewer/session/:id/refreshOtherText/:lastOtherUpdateTime', secured, function(req,res){
+	log(LOG_TRACE, 'Interviewer: checking for candidate text update');
 	res.send( getRefreshedText(sessions.get(req.params.id), req.params.lastOtherUpdateTime, "candidate") );
 });
 
 app.post('/interviewer/session/:id/updateMyText', secured, function(req,res){
+	//console.log('Intervierwer: update my text ['+req.params.id+'], body ['+req.body.myText+']');
 	var s = sessions.get(req.params.id);
 	if (s != null && s.open)
 	{
 		s.interviewerText = req.body.myText;
 		s.interviewerTextLastUpdateTime = new Date();
 		sessions.update(s);
+		log(LOG_DEBUG,'Interviewer: updated session <'+s.id+'> with new interviewer text');
+	}
+	else
+	{
+		log(LOG_DEBUG,'Interviewer: cannot update interviewer text, session null or closed!');
 	}
 
 	res.send( getRefreshedText(s, req.body.lastOtherUpdateTime, "candidate") );
+});
+
+app.post('/interviewer/session/:id/updateMyComments', secured, function(req,res){
+	var s = sessions.get(req.params.id);
+	if (s != null && s.open)
+	{
+		s.interviewerComments = req.body.myComments;
+		sessions.update(s);
+	}
 });
 
 
@@ -707,6 +754,7 @@ app.get('/candidate/session/:id/closed', function(req,res){
 
 
 app.get('/candidate/session/:id/refreshOtherText/:lastOtherUpdateTime', function(req,res){
+	log(LOG_TRACE, 'Candidate: checking for interviewer text update');
 	res.send( getRefreshedText(sessions.get(req.params.id), req.params.lastOtherUpdateTime, "interviewer") );
 });
 
@@ -717,20 +765,18 @@ app.post('/candidate/session/:id/updateMyText', function(req,res){
 		s.candidateText = req.body.myText;
 		s.candidateTextLastUpdateTime = new Date();
 		sessions.update(s);
+		log(LOG_DEBUG,'Candidate: updated session <'+s.id+'> with new candidate text');
+	}
+	else
+	{
+		log(LOG_DEBUG,'Candidate: cannot update candidate text, session null or closed!');
 	}
 
 	res.send( getRefreshedText(s, req.body.lastOtherUpdateTime, "interviewer") );
 });
 
-
-
-// ===============================================
-// START SERVER
-// ===============================================
-if  (app.settings.env != 'test') {
-	app.listen(port);
-	console.log("Server listening on port %d in %s mode", app.address().port, app.settings.env);
-} else {
-	app.url = 'http://localhost:'+port;
+	return app;
 }
+
+module.exports.app = app;
 
